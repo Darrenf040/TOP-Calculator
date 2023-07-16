75c55a40510a58eb82a8883a3e59de652bee7205
Darrenf040 <darrenfr83@gmail.com>
2023-07-15 21:54:49 -0500

    math operation functions + function for operator input

--- script.js
+++ script.js
@@ -23,3 +23,55 @@ const decimal = document.createElement("button");
 decimal.textContent = ".";
 numbersContainer.appendChild(decimal);
 
+function add(...nums){
+    let sum = 0;
+    nums.forEach(num => {
+        sum = sum + num;
+    });
+    return sum;
+}
+function subtract(...nums){
+    let sum = nums[0];
+    for(let i = 1; i < nums.length; i++){
+        sum = sum - nums[i];
+    }
+    return sum;
+}
+function multiply(...nums){
+    let sum = 1;
+    nums.forEach(num => {
+        sum = sum * num;
+    });
+    return sum;
+}
+function divide(...nums){
+    let sum = nums[0];
+    for(let i = 1; i < nums.length; i++){
+        sum = sum / nums[i];
+    }
+    return sum;
+}
+// console.log(add(1, 2, 3, 4, 5));
+// console.log(subtract(10, 5, 1, 1, 1));
+// console.log(multiply(2, 2, 2));
+// console.log(divide(20, 2, 2, 2, 2));
+
+function operate(op, num1, num2){
+    if(op == "+"){
+        return add(num1, num2);
+    }
+    else if(op == "-"){
+        return subtract(num1, num2);
+    }
+    else if(op == "*" || op == "x"){
+        return multiply(num1, num2);
+    }
+    else if(op == "/"){
+        return divide(num1, num2);
+    }
+}
+
+console.log(operate("+", 1, 2));
+console.log(operate("-", 1, 2));
+console.log(operate("/", 1, 2));
+console.log(operate("x", 1, 2));
